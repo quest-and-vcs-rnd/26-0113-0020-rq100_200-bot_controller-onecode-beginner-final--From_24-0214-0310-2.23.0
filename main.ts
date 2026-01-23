08958e0d81de1d2b96bc671645174ba00afadee4
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2026-01-22 19:59:05 -0800

    *** 'Pull changes' not work so manually copy from Github into main.ts *** Keep pxt.json as-is or will lose manual copy *** Chrome broke down so using MsEdge

--- main.ts
+++ main.ts
@@ -1668,7 +1668,7 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    _system_Github_VER_STRING = "v0.2.2"
+    _system_Github_VER_STRING = "v0.5.2"
     quest_Note_1.quest_Show_String_For_Note_Small_Func(
         "©️ 2026 Quest Institute. All rights reserved."
     )
